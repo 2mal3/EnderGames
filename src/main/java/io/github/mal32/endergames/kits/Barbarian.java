7e070a7781f778ff595bef66fa29e6f442c2feed
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-05-10 00:44:23 +0200

    feat(barbarian): feedback on strong hit
    closes #15

--- src/main/java/io/github/mal32/endergames/kits/Barbarian.java
+++ src/main/java/io/github/mal32/endergames/kits/Barbarian.java
@@ -1,5 +1,8 @@
 package io.github.mal32.endergames.kits;
 
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.Tag;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -28,5 +31,13 @@ public class Barbarian extends AbstractKit {
     final int maxFoodLevel = 20;
     double damageMultiplier = 1 + ((maxFoodLevel - foodLevel) * 0.05);
     event.setDamage(event.getDamage() * damageMultiplier);
+
+    if (damageMultiplier > 1.30) {
+      Location location = event.getEntity().getLocation();
+      location.getWorld().playSound(location, Sound.BLOCK_MANGROVE_ROOTS_BREAK, 1, 0.5f);
+      location
+          .getWorld()
+          .spawnParticle(Particle.INFESTED, location.clone().add(0, 1, 0), 20, 0.2, 0.6, 0.2, 2);
+    }
   }
 }
