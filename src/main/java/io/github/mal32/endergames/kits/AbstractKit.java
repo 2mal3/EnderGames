7f31262a39a711578a7346412bad535030cf9350
6mal7 <184911647+6mal7@users.noreply.github.com>
2025-05-13 16:03:25 +0200

    feat: equip equipment

--- src/main/java/io/github/mal32/endergames/kits/AbstractKit.java
+++ src/main/java/io/github/mal32/endergames/kits/AbstractKit.java
@@ -1,11 +1,17 @@
 package io.github.mal32.endergames.kits;
 
 import io.github.mal32.endergames.phases.game.AbstractModule;
+
+import java.util.Collection;
 import java.util.Objects;
+import java.util.Random;
+
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.loot.LootContext;
+import org.bukkit.loot.LootTable;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -23,9 +29,27 @@ public abstract class AbstractKit extends AbstractModule {
   }
 
   public void start(Player player) {
-    Bukkit.dispatchCommand(
-        Bukkit.getConsoleSender(),
-        "loot give " + player.getName() + " loot enga:kits/" + getName());
+    LootTable lootTable = Bukkit.getLootTable(new NamespacedKey("enga", "kits/" + getName()));
+
+    Collection<ItemStack> loot = lootTable != null ? lootTable.populateLoot(new Random(), new LootContext.Builder(player.getLocation()).build()) : null;
+
+    if (loot == null) return;
+
+    for (ItemStack item : loot) {
+      if (item == null) return;
+      String name = item.getType().name();
+      if (name.endsWith("_HELMET")) {
+        player.getInventory().setHelmet(item);
+      } else if (name.endsWith("_CHESTPLATE")) {
+        player.getInventory().setChestplate(item);
+      } else if (name.endsWith("_LEGGINGS")) {
+        player.getInventory().setLeggings(item);
+      } else if (name.endsWith("_BOOTS")) {
+        player.getInventory().setBoots(item);
+      } else {
+        player.getInventory().addItem(item);
+      }
+    }
   }
 
   public String getName() {
