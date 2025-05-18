ae3c63e463d548f45830d65d36a6d40d597c506e
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-05-18 16:59:00 +0200

    chore: higher game world pre gen radious

--- src/main/java/io/github/mal32/endergames/worlds/game/LoadPhase.java
+++ src/main/java/io/github/mal32/endergames/worlds/game/LoadPhase.java
@@ -18,11 +18,13 @@ public class LoadPhase extends AbstractPhase {
     for (Chunk chunk : world.getLoadedChunks()) {
       chunk.removePluginChunkTicket(plugin);
     }
+
+    final int loadRadius = 4;
     for (int x = (int) (spawnLocation.getX() - (2 * 16));
-        x < spawnLocation.getX() + (2 * 16);
+        x < spawnLocation.getX() + (loadRadius * 16);
         x += 16) {
       for (int z = (int) (spawnLocation.getZ() - (2 * 16));
-          z < spawnLocation.getZ() + (2 * 16);
+          z < spawnLocation.getZ() + (loadRadius * 16);
           z += 16) {
         world
             .getChunkAt(new Location(world, x, spawnLocation.getY(), z))
