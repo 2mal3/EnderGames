b059d0cb4ab2b2cbb337e4bc2aad5663885a7ea3
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-05-28 15:27:37 +0200

    chore: improve chunk pre gen
    - cancel world pre gen when game starts
    - spiral chunk loading pattern
    
    closes #57

--- src/main/java/io/github/mal32/endergames/worlds/game/LoadPhase.java
+++ src/main/java/io/github/mal32/endergames/worlds/game/LoadPhase.java
@@ -1,40 +1,62 @@
 package io.github.mal32.endergames.worlds.game;
 
 import io.github.mal32.endergames.EnderGames;
+import java.util.ArrayList;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.scheduler.BukkitTask;
 
 public class LoadPhase extends AbstractPhase {
+  private final ArrayList<BukkitTask> loadTasks = new ArrayList<>();
+
   public LoadPhase(EnderGames plugin, GameManager manager, Location spawnLocation) {
     super(plugin, manager, spawnLocation);
 
-    Bukkit.getScheduler().runTaskLater(this.plugin, this::loadSpawnChunks, 20 * 5);
+    var startLoadTask =
+        Bukkit.getScheduler().runTaskLater(this.plugin, this::loadSpawnChunks, 20 * 5);
+    loadTasks.add(startLoadTask);
   }
 
   private void loadSpawnChunks() {
-    final int loadRadius = 4;
-    int loadDelayTicks = 0;
-    final int loadDelayIncrease = 5;
-    var world = spawnLocation.getWorld();
+    final int loadRadius = 8;
+    var location = spawnLocation.clone();
 
-    for (int x = (int) (spawnLocation.getX() - (loadRadius * 16));
-        x < spawnLocation.getX() + (loadRadius * 16);
-        x += 16) {
-      for (int z = (int) (spawnLocation.getZ() - (loadRadius * 16));
-          z < spawnLocation.getZ() + (loadRadius * 16);
-          z += 16) {
-        final Location location = new Location(world, x, spawnLocation.getY(), z);
-        final int currentLoadDelayTicks = loadDelayTicks;
+    int invert = 1;
+    for (int i = 0; i < loadRadius; i++) {
+      for (int k = 0; k < i; k++) {
+        location.add(invert * 16, 0, 0);
+        scheduleChunkLoad(location.clone());
+      }
+      for (int k = 0; k < i; k++) {
+        location.add(0, 0, invert * 16);
+        scheduleChunkLoad(location.clone());
+      }
+
+      invert *= -1;
+    }
+  }
+
+  int loadDelayTicks = 0;
+  final int loadDelayIncrease = 5;
+
+  private void scheduleChunkLoad(Location location) {
+    var chunkLoadTask =
         Bukkit.getScheduler()
             .runTaskLater(
-                plugin, () -> world.getChunkAt(location).load(true), currentLoadDelayTicks);
-        loadDelayTicks += loadDelayIncrease;
-      }
-    }
+                plugin, () -> location.getWorld().getChunkAt(location).load(true), loadDelayTicks);
+    loadTasks.add(chunkLoadTask);
+
+    loadDelayTicks += loadDelayIncrease;
   }
 
   @Override
   public void disable() {
     super.disable();
+
+    for (BukkitTask task : loadTasks) {
+      if (task == null || task.isCancelled()) continue;
+
+      task.cancel();
+    }
   }
 }
