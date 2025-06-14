113da9e61cd001de10fc0a0c1fdffe074d59e0d0
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-06-14 18:09:57 +0200

    fix(cat): wrong in game description of hand damage

--- src/main/java/io/github/mal32/endergames/kits/Cat.java
+++ src/main/java/io/github/mal32/endergames/kits/Cat.java
@@ -79,7 +79,7 @@ public class Cat extends AbstractKit {
     return new KitDescriptionItem(
         Material.COD,
         "Cat",
-        "Gains Speed III for 30 seconds when eating raw fish. It deals +1 damage with bare hands"
+        "Gains Speed III for 30 seconds when eating raw fish. It deals +2 damage with bare hands"
             + " and takes 50% less fall damage.",
         "20 raw fish",
         Difficulty.EASY);
