80bf47008515bbad8f9a24c95e604c6794f41a09
MCsharerGIT <98043315+MCsharerGIT@users.noreply.github.com>
2025-06-15 01:51:33 +0200

    Lucker Description Change

--- src/main/java/io/github/mal32/endergames/kits/Lucker.java
+++ src/main/java/io/github/mal32/endergames/kits/Lucker.java
@@ -327,7 +327,7 @@ public class Lucker extends AbstractKit {
     return new KitDescriptionItem(
             Material.AZALEA,
             "Lucker",
-            "No special abilities, but is blessed with extraordinary luck. (Better chest loot, more luck when fishing, mining, existing,...)",
+            "Blessed with extraordinary luck. (Better chest loot, more luck when fishing, mining, existing,...)",
             "Light-Green Leather Chestplate",
             Difficulty.MEDIUM);
   }
