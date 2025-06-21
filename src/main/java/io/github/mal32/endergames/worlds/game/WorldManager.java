f76058059d4b8d9764945d3ddab46b5873546673
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-06-21 17:53:16 +0200

    chore: disable weather
    closes #105

--- src/main/java/io/github/mal32/endergames/worlds/game/WorldManager.java
+++ src/main/java/io/github/mal32/endergames/worlds/game/WorldManager.java
@@ -19,6 +19,10 @@ public class WorldManager {
     border.setWarningTime(60);
     border.setDamageBuffer(1);
 
+    world.setStorm(false);
+    world.setThundering(false);
+    world.setGameRule(GameRule.DO_WEATHER_CYCLE, false);
+
     if (!world.getPersistentDataContainer().has(this.spawnLocationKey)) {
       plugin.getComponentLogger().info("Creating spawn location");
       spawnLocation = new Location(world, 0, 200, 0);
