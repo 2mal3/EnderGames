d87697613942f0e2e0fc43655328419cca936bb6
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-08-27 17:59:24 +0200

    fix: bird drops its elytra

--- src/main/java/io/github/mal32/endergames/kits/Bird.java
+++ src/main/java/io/github/mal32/endergames/kits/Bird.java
@@ -4,7 +4,9 @@ import io.github.mal32.endergames.EnderGames;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class Bird extends AbstractKit {
@@ -29,6 +31,14 @@ public class Bird extends AbstractKit {
     if (!playerCanUseThisKit(killer)) return;
 
     killer.getInventory().addItem(new ItemStack(Material.FIREWORK_ROCKET, 5));
+  }
+
+  @EventHandler(priority = EventPriority.LOW)
+  public void onPlayerDeath(PlayerDeathEvent event) {
+    Player player = event.getEntity();
+    if (!playerCanUseThisKit(player)) return;
+
+    player.getInventory().remove(Material.ELYTRA);
   }
 
   @Override
