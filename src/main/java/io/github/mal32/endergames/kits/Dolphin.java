4d20f14add5fbf741c90798552c06e0865ce2a6f
MCsharerGIT <98043315+MCsharerGIT@users.noreply.github.com>
2026-03-05 22:42:06 +0100

    Dolphin Jump while in water instead of dolphins grace
    issue: still need to remove dolphins grace

--- src/main/java/io/github/mal32/endergames/kits/Dolphin.java
+++ src/main/java/io/github/mal32/endergames/kits/Dolphin.java
@@ -1,15 +1,21 @@
 package io.github.mal32.endergames.kits;
 
 import io.github.mal32.endergames.EnderGames;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.Style;
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
 
 public class Dolphin extends AbstractKit {
   public Dolphin(EnderGames plugin) {
@@ -49,7 +55,33 @@ public class Dolphin extends AbstractKit {
         "loot give " + player.getName() + " loot minecraft:gameplay/fishing/fish");
   }
 
-  @Override
+  // Dolphin Jump While in water
+  @EventHandler
+  public void onPlayerToggleFlight(PlayerToggleFlightEvent event) {
+    var player = event.getPlayer();
+    if (!playerCanUseThisKit(player)) return;
+    if (!playerisinwater(player)) return;
+
+    event.setCancelled(true);
+    player.setFlying(false);
+
+    player.setAllowFlight(false);
+
+    // The actual jump, reduced by slowness
+    int slownessLevel = 0;
+    if (player.getPotionEffect(PotionEffectType.SLOWNESS) != null) {
+      slownessLevel = player.getPotionEffect(PotionEffectType.SLOWNESS).getAmplifier() + 1;
+    }
+    double horizontalMultiplier = HORIZONTAL_JUMP_SPEED * (1 - 0.30 * slownessLevel);
+    double verticalMultiplier = VERTICAL_JUMP_SPEED * (1 - 0.30 * slownessLevel);
+    Vector jump =
+        player.getLocation().getDirection().multiply(horizontalMultiplier).setY(verticalMultiplier);
+    player.setVelocity(jump);
+
+    player.getWorld().playSound(player.getLocation(), Sound.ENTITY_WITHER_SHOOT, 1, 1);
+}
+
+    @Override
   public KitDescription getDescription() {
     return new KitDescription(
         Material.TROPICAL_FISH,
