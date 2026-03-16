11991af63dd8ac0e7f60b60f7a0ca9090a97141a
lambdaphoenix <184911647+lambdaphoenix@users.noreply.github.com>
2026-03-16 23:01:12 +0100

    refactor: use plugin version to display exact version (#218)
    Co-authored-by: 2mal3 <56305732+2mal3@users.noreply.github.com>

--- src/main/java/io/github/mal32/endergames/EnderGames.java
+++ src/main/java/io/github/mal32/endergames/EnderGames.java
@@ -1,25 +1,14 @@
 package io.github.mal32.endergames;
 
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.github.lambdaphoenix.advancementLib.AdvancementAPI;
 import io.github.mal32.endergames.game.phases.PhaseController;
 import io.github.mal32.endergames.kits.KitRegistry;
 import io.github.mal32.endergames.lobby.MapManager;
 import io.github.mal32.endergames.lobby.PlayerDifficulty;
 import io.github.mal32.endergames.lobby.items.MenuManager;
-import io.papermc.paper.command.brigadier.CommandSourceStack;
-import io.papermc.paper.command.brigadier.Commands;
-import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import org.bstats.bukkit.Metrics;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EnderGames extends JavaPlugin {
@@ -61,7 +50,6 @@ public class EnderGames extends JavaPlugin {
 
     if (isInDebugMode()) {
       this.getComponentLogger().warn("Debug mode is enabled.");
-      this.getComponentLogger().info(Component.text("Loaded Version ").append(getExactVersion()));
     } else {
       final int PLUGIN_ID = 25844;
       var metrics = new Metrics(this, PLUGIN_ID);
@@ -81,11 +69,6 @@ public class EnderGames extends JavaPlugin {
 
     KDScoreboard kdScoreboard = new KDScoreboard(this);
 
-    this.getLifecycleManager()
-        .registerEventHandler(
-            LifecycleEvents.COMMANDS,
-            commands -> commands.registrar().register(endergamesCommand()));
-
     this.registerKitAdvancements();
   }
 
@@ -94,46 +77,6 @@ public class EnderGames extends JavaPlugin {
     for (var kit : KitRegistry.getKits()) {
       kit.registerAdvancement(advancementAPI);
     }
-  }
-
-  private LiteralCommandNode<CommandSourceStack> endergamesCommand() {
-    return Commands.literal("endergames")
-        .then(Commands.literal("version").executes(this::getVersionCommand))
-        .build();
-  }
-
-  private int getVersionCommand(CommandContext<CommandSourceStack> ctx) {
-    if (!(ctx.getSource().getSender() instanceof Player player)) return -1;
-
-    Component versionText = getExactVersion();
-    Component text =
-        Component.text("EnderGames Version: ", NamedTextColor.YELLOW)
-            .append(versionText.color(NamedTextColor.GOLD));
-
-    player.sendMessage(text);
-
-    return Command.SINGLE_SUCCESS;
-  }
-
-  private Component getExactVersion() {
-    Properties props = new Properties();
-    try {
-      props.load(getClass().getClassLoader().getResourceAsStream("git.properties"));
-    } catch (IOException e) {
-      return Component.text("Could not load version information", NamedTextColor.RED);
-    }
-
-    String commitHash = props.getProperty("git.commit.id.abbrev");
-    String commitBranch = props.getProperty("git.branch");
-    String currentTag = props.getProperty("git.closest.tag.name");
-    boolean isDirty = Boolean.parseBoolean(props.getProperty("git.dirty"));
-
-    Component text = Component.text(currentTag + "-" + commitHash + "-" + commitBranch);
-    if (isDirty) {
-      text = text.append(Component.text("-dirty"));
-    }
-
-    return text;
   }
 
   @Override
