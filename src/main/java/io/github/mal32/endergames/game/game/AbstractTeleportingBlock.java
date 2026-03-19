876dda5cd7f67dc6d69e69aa5e120bb3bb0abab5
2mal3 <56305732+2mal3@users.noreply.github.com>
2026-03-19 17:09:06 +0100

    chore(TeleportingBlocks): allow blocks to fall through leaves (#232)
    hopefully fixes #185

--- src/main/java/io/github/mal32/endergames/game/game/AbstractTeleportingBlock.java
+++ src/main/java/io/github/mal32/endergames/game/game/AbstractTeleportingBlock.java
@@ -2,7 +2,6 @@ package io.github.mal32.endergames.game.game;
 
 import io.github.mal32.endergames.EnderGames;
 import org.bukkit.Bukkit;
-import org.bukkit.HeightMap;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
@@ -29,9 +28,6 @@ public abstract class AbstractTeleportingBlock {
 
   private void place() {
     loadChunkIfNotLoaded();
-
-    int y = world.getHighestBlockAt(location, HeightMap.OCEAN_FLOOR).getY();
-    location.setY(y + 1);
 
     Location blockSpawnLocation = this.location.getBlock().getLocation().clone();
     blockSpawnLocation.setY(256);
