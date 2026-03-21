e20f59916309d8b4d130169e3c6fda21c0552c97
lambdaphoenix <184911647+lambdaphoenix@users.noreply.github.com>
2026-03-21 23:17:03 +0100

    feat(tests): add tests for KitUtils

---
+++ src/test/java/io/github/mal32/endergames/kitsystem/util/KitUtilsTest.java
@@ -0,0 +1,34 @@
+package io.github.mal32.endergames.kitsystem.util;
+
+import io.github.mal32.endergames.BaseMockBukkitTest;
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class KitUtilsTest extends BaseMockBukkitTest {
+  @Test
+  void enchantItem() {
+    ItemStack item = ItemStack.of(Material.WOODEN_SWORD);
+
+    ItemStack enchanted = KitUtils.enchantItem(item, Enchantment.UNBREAKING, 1);
+
+    assertTrue(enchanted.getEnchantments().containsKey(Enchantment.UNBREAKING));
+    assertEquals(1, enchanted.getEnchantmentLevel(Enchantment.UNBREAKING));
+    }
+
+  @Test
+  void colorLeatherArmor() {
+    ItemStack leggings = ItemStack.of(Material.LEATHER_LEGGINGS);
+    Color color = Color.fromRGB(3064446);
+
+    ItemStack colored = KitUtils.colorLeatherArmor(leggings, color);
+
+    assertInstanceOf(LeatherArmorMeta.class, colored.getItemMeta());
+    LeatherArmorMeta meta = (LeatherArmorMeta) colored.getItemMeta();
+    assertEquals(color, meta.getColor());
+    }
+}
