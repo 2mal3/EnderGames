20ec2117c5eaf8f9269e28147e3b77e25d87b937
lambdaphoenix <184911647+lambdaphoenix@users.noreply.github.com>
2026-03-21 16:31:11 +0100

    feat: use LinkedHashMap for insertion-sorted kits

--- src/main/java/io/github/mal32/endergames/kitsystem/api/KitManager.java
+++ src/main/java/io/github/mal32/endergames/kitsystem/api/KitManager.java
@@ -6,7 +6,7 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.Plugin;
 
 public class KitManager {
-  private final Map<String, AbstractKit> kits = new HashMap<>(32);
+  private final Map<String, AbstractKit> kits = new LinkedHashMap<>(32);
   private final Plugin plugin;
   private final Set<AbstractKit> active = new HashSet<>();
 
