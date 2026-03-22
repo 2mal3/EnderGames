931325241ba71f7ded7f208481369a54a8c2a2f9
lambdaphoenix <184911647+lambdaphoenix@users.noreply.github.com>
2026-03-22 13:57:37 +0100

    feat: add Javadoc for KitRegistry

--- src/main/java/io/github/mal32/endergames/kitsystem/registry/KitRegistry.java
+++ src/main/java/io/github/mal32/endergames/kitsystem/registry/KitRegistry.java
@@ -5,7 +5,29 @@ import io.github.mal32.endergames.kitsystem.api.KitManager;
 import io.github.mal32.endergames.kitsystem.api.KitService;
 import io.github.mal32.endergames.kitsystem.kits.*;
 
+/**
+ * Central registry for all available kits in the EnderGames plugin.
+ *
+ * <p>This class is responsible for instantiating every kit and registering it with the global
+ * {@link KitManager}. It is invoked once during plugin initialization to make all kits available to
+ * the kit system.
+ *
+ * <h2>Responsibilities</h2>
+ *
+ * <ul>
+ *   <li>Create instances of all kit classes
+ *   <li>Register each kit with the {@link KitManager}
+ *   <li>Provide the {@link KitService} instance to each kit
+ * </ul>
+ *
+ * <p>All kits should be added here to become selectable and usable in-game.
+ */
 public final class KitRegistry {
+  /**
+   * Registers all available kits with the plugin's {@link KitManager}.
+   *
+   * @param plugin the main plugin instance providing access to the kit system
+   */
   public static void registerAll(EnderGames plugin) {
     final KitManager kitManager = plugin.getKitSystem().manager();
     final KitService kitService = plugin.getKitSystem().service();
